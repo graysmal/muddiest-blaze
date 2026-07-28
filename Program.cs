d5178f1b667b379249fc6a7e76eee494c4ac3ce5
graysmal <graycsmal@gmail.com>
2026-07-28 00:05:05 -0500

    change log sink from PostgreSQL to Loki.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 using BlazorApp1.Components;
+using BlazorApp1.Context;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using MudBlazor.Services;
-using NpgsqlTypes;
 using Serilog;
+using Serilog.Core;
 using Serilog.Enrichers;
-using Serilog.Events;
-using Serilog.Sinks.PostgreSQL;
-using System.Security.Claims;
-using BlazorApp1.Context;
 using Serilog.Enrichers.Span;
-using Microsoft.EntityFrameworkCore;
+using Serilog.Events;
+using Serilog.Sinks.Grafana.Loki;
+using System.Security.Claims;
+
 
 // gets Name and preferred_username claims for OIDC log events.
 static (string? Name, string? PreferredUsername) GetLoggingClaims(ClaimsPrincipal? principal)
@@ -27,47 +28,28 @@ static (string? Name, string? PreferredUsername) GetLoggingClaims(ClaimsPrincipa
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-builder.Services.AddMudServices();
-
 // https://codewithmukesh.com/blog/structured-logging-with-serilog-in-aspnet-core/
+// https://github.com/b00ted/serilog-sinks-postgresql
+// https://github.com/serilog-contrib/serilog-sinks-grafana-loki
 // used fluent api rather than appsettings.json because the json object is pretty gross,
 // and there are likely not many changes to be made to these settings per deployment.
-// https://github.com/b00ted/serilog-sinks-postgresql
-IDictionary<string, ColumnWriterBase> columnWriters = new Dictionary<string, ColumnWriterBase>
-{
-    {"timestamp", new TimestampColumnWriter() },
-    {"level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
-    {"name", new SinglePropertyColumnWriter("Name", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"preferred_username", new SinglePropertyColumnWriter("preferred_username", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"client_ip", new SinglePropertyColumnWriter("ClientIp", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"machine_name", new SinglePropertyColumnWriter("MachineName", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"user_agent", new SinglePropertyColumnWriter("UserAgent", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"request_path", new SinglePropertyColumnWriter("RequestPath", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"request_method", new SinglePropertyColumnWriter("RequestMethod", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"status_code", new SinglePropertyColumnWriter("StatusCode", PropertyWriteMethod.Raw, NpgsqlDbType.Integer, "l") },
-    {"elapsed_ms", new SinglePropertyColumnWriter("Elapsed", PropertyWriteMethod.Raw, NpgsqlDbType.Double, "l") },
-    {"trace_id", new SinglePropertyColumnWriter("TraceId", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l")},
-    {"request_id", new SinglePropertyColumnWriter("RequestId", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") },
-    {"message", new RenderedMessageColumnWriter() },
-    {"message_template", new MessageTemplateColumnWriter() },
-    {"exception", new ExceptionColumnWriter() },
-    {"properties", new LogEventSerializedColumnWriter() }
-    
-};
-const string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Name} ({preferred_username}, {ClientIp}, {MachineName}) trace:{TraceId} req:{RequestId} {Message:lj}{NewLine}{Exception}";
+
+var levelSwitch =
+    new LoggingLevelSwitch(builder.Configuration.GetValue("Logging:MinimumLevel", LogEventLevel.Information));
+
+const string fileOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Name} ({preferred_username}, {ClientIp}, {MachineName}) trace:{TraceId} req:{RequestId} {Message:lj}{NewLine}{Exception}";
 builder.Services.AddSerilog((services, lc) => lc
-    .MinimumLevel.Information()
+    .MinimumLevel.ControlledBy(levelSwitch)
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
     .WriteTo.Console()
-    .WriteTo.File("./logs/muddiest.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit:null, outputTemplate:outputTemplate)
-    .WriteTo.PostgreSQL(builder.Configuration.GetConnectionString("PostgreSQL"), "Log", columnWriters,
-        needAutoCreateTable: true)
+    .WriteTo.File(
+        "./logs/muddiest.log", rollingInterval: RollingInterval.Day, 
+        retainedFileCountLimit:null, outputTemplate:fileOutputTemplate)
+    .WriteTo.GrafanaLoki(
+        builder.Configuration.GetValue<string>("Loki:uri"), 
+        [new LokiLabel { Key="app", Value="web_app"}])
     .Enrich.FromLogContext()
     .Enrich.WithClientIp(IpVersionPreference.Ipv4Only)
-    .Enrich.WithCorrelationId()
     .Enrich.WithSpan()
     .Enrich.WithRequestHeader("User-Agent")
     .Enrich.WithUserClaims("Name", "preferred_username")
@@ -119,6 +101,11 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             }
         };
     });
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 builder.Services.AddAuthorization();
 builder.Services.AddControllersWithViews()
     .AddMicrosoftIdentityUI();
@@ -155,10 +142,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
-
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
